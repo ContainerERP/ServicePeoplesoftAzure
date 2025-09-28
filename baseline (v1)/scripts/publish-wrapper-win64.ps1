52ba418bc0b29928eba03dbda385c0f9d59e3372
manandaram <manandaram@yahoo.com>
2025-09-28 06:19:09 -0700

    Restore clean layout: wrappers/peoplesoft-http/src/{Wrapper.Api,Wrapper.Core}; drop publish & stray folder

--- baseline (v1)/scripts/publish-wrapper-win64.ps1
+++
@@ -1,6 +0,0 @@
-param(
-  [string]$ProjPath = "wrappers\peoplesoft-http\ServicePeoplesoftAzure-Wrapper.csproj",
-  [string]$OutDir = "publish\wrapper-win64"
-)
-dotnet publish $ProjPath -c Release -r win-x64 --self-contained:false -o $OutDir
-Write-Host "Published to $OutDir"
